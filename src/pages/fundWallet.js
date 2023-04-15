5f5b2c1f3d7e29d43e1bc77166efa0c8f70a264b
Olasunkanmi Usman <Olasunkanmiusman1111@gmail.com>
2023-04-15 18:25:20 +0100

    added breakwords to fundwallet page

--- src/pages/fundWallet.js
+++ src/pages/fundWallet.js
@@ -1,6 +1,6 @@
 import WemaBank from "../images/wema-bank-plc-logo-vector.png"
 const FundWallet = () => {
-    return<div className="bg-blue-900 md:mx-3 lg:mx-8 mx-auto p-5 md:p-12 text-white rounded-3xl my-24 w-11/12 ">
+    return<div className="bg-blue-900 md:mx-3 lg:mx-8 mx-auto p-5 break-words md:p-12 text-white rounded-3xl my-24 w-11/12 ">
         <img src={WemaBank} className='w-24' alt="wemaBankLogo"/>
     <h1 className="my-6 text-xl">Account Number: 8870939987</h1>
     <div className="md:flex justify-between my-5 text-lg">
